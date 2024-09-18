91a5294af6fc47462f2144fc569759c916e4784a
MaxShvachko <maxshvachko@ukr.net>
2024-09-18 16:49:39 +0300

    Update envs for the vue router

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/invoice-app/',
+  // eslint-disable-next-line no-undef
+  base: process.env.BASE_URL,
   plugins: [vue()],
   build: {
     rollupOptions: {
